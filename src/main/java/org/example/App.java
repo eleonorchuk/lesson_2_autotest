b081be5907e3f92caa1a45caa935daa3268a87ca
eleonorchuk <zoimy@ukr.net>
2021-12-15 23:47:13 +0200

    lesson_2_autotest/lesson_5/

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -16,57 +16,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class App 
 {
     public static void main(String[] args) {
-        scenario1();
-        scenario2();
-        scenario3();
-    }
-
-    // Click Kontakti
-    public static void scenario1() {
-        System.setProperty(
-                "webdriver.chrome.driver", "src/main/resources/chromedriver.exe"
-        );
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://bi.ua/");
-        driver.manage().window().setSize(new Dimension(1386, 700));
-        WebElement webElement = driver.findElement(By.xpath(
-                "//a[@href=\"/ukr/help/contacts/\"and@ class=\"linkJ\"]"
-        ));
-        webElement.click();
-        driver.close();
-    }
-
-    // Click Oplata i Dostavka
-    public static void scenario2() {
-        System.setProperty(
-                "webdriver.chrome.driver", "src/main/resources/chromedriver.exe"
-        );
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://bi.ua/");
-        driver.manage().window().setSize(new Dimension(1386, 700));
-        WebElement webElement = driver.findElement(By.xpath(
-                "//a[@ href=\"/ukr/help/how_to_order/\"and@ class=\"linkJ\"]"
-        ));
-        webElement.click();
-
-    }
-
-    // Show Nova Pochta details
-    public static void scenario3() {
-        System.setProperty(
-                "webdriver.chrome.driver", "src/main/resources/chromedriver.exe"
-        );
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://bi.ua/");
-        driver.manage().window().setSize(new Dimension(1386, 700));
-        WebElement webElement = driver.findElement(By.xpath(
-                "//a[@ href=\"/ukr/help/how_to_order/\"and@ class=\"linkJ\"]"
-        ));
-        webElement.click();
-
-        WebElement novPochtaElem = driver.findElement(By.xpath( "//button[@ id=\"btnNovaUkr\"]"));
-        novPochtaElem.click();
-
     }
 }
 
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -2,13 +2,20 @@ package org.example;
 
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
+    private static Logger logger = LoggerFactory.getLogger(AppTest.class);
     /**
      * Rigorous Test :-)
      */
@@ -16,5 +23,96 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    // Click Kontakti
+    @Test
+    public void scenario1Test() {
+        System.setProperty(
+                "webdriver.chrome.driver", "src/main/resources/chromedriver.exe"
+        );
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://bi.ua/");
+        Cookie cookie = driver.manage().getCookieNamed("testing");
+        if (cookie == null)
+            driver.manage().addCookie(new Cookie("testing", "test"));
+
+        driver.manage().window().setSize(new Dimension(1386, 700));
+        WebElement webElement = driver.findElement(By.xpath(
+                "//a[@href=\"/ukr/help/contacts/\"and@ class=\"linkJ\"]"
+        ));
+        webElement.click();
+        String url = driver.getCurrentUrl();
+        assertTrue(url.equals("https://bi.ua/ukr/help/contacts/"));
+        driver.close();
+    }
+
+    // Click Oplata i Dostavka
+    @Test
+    public void scenario2Test() {
+        System.setProperty(
+                "webdriver.chrome.driver", "src/main/resources/chromedriver.exe"
+        );
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://bi.ua/");
+        Cookie cookie = driver.manage().getCookieNamed("testing");
+        if (cookie == null)
+            driver.manage().addCookie(new Cookie("testing", "test"));
+
+        driver.manage().window().setSize(new Dimension(1386, 700));
+        WebElement webElement = driver.findElement(By.xpath(
+                "//a[@ href=\"/ukr/help/how_to_order/\"and@ class=\"linkJ\"]"
+        ));
+        webElement.click();
+        String url = driver.getCurrentUrl();
+        assertTrue(url.equals("https://bi.ua/ukr/help/how_to_order/"));
+        driver.close();
+    }
+
+    // Show Nova Pochta details
+    @Test
+    public void scenario3Test() {
+        System.setProperty(
+                "webdriver.chrome.driver", "src/main/resources/chromedriver.exe"
+        );
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://bi.ua/");
+        Cookie cookie = driver.manage().getCookieNamed("testing");
+        if (cookie == null)
+            driver.manage().addCookie(new Cookie("testing", "test"));
+
+        driver.manage().window().setSize(new Dimension(1386, 700));
+        WebElement webElement = driver.findElement(By.xpath(
+                "//a[@ href=\"/ukr/help/how_to_order/\"and@ class=\"linkJ\"]"
+        ));
+        webElement.click();
+
+        WebElement novPochtaElem = driver.findElement(By.xpath( "//button[@ id=\"btnNovaUkr\"]"));
+        novPochtaElem.click();
+        String url = driver.getCurrentUrl();
+        assertTrue(url.equals("https://bi.ua/ukr/help/how_to_order/"));
+        driver.close();
+    }
+
+    // ?
+    @Test
+    public void scenario4Test() {
+        System.setProperty(
+                "webdriver.chrome.driver", "src/main/resources/chromedriver.exe"
+        );
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://bi.ua/");
+        Cookie cookie = driver.manage().getCookieNamed("testing");
+        if (cookie == null)
+            driver.manage().addCookie(new Cookie("testing", "test"));
+
+        driver.manage().window().setSize(new Dimension(1386, 700));
+        WebElement webElement = driver.findElement(By.xpath(
+                "//a[@id=\"571345/1\"]"
+        ));
+        webElement.click();
+        String url = driver.getCurrentUrl();
+        assertTrue(url.equals("https://bi.ua/ukr/product/elochnaya-igrushka-novogodko-gnom-podveska-s-led-13-sm-974192.html"));
+        driver.close();
     }
 }
